e3db250039fea1ebf694c168cef70cfa781b4786
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-06-02 18:08:23 +0530

    completed the home section

--- src/Componont/Home/Homepage/Casestudies/Card.jsx
+++ src/Componont/Home/Homepage/Casestudies/Card.jsx
@@ -1,100 +1,108 @@
 import React from "react";
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemButton from '@mui/material/ListItemButton';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import CloseIcon from '@mui/icons-material/Close';
-import Slide from '@mui/material/Slide';
-
-
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="right" ref={ref} {...props} />;
-  });
+  return <Slide direction="right" ref={ref} {...props} />;
+});
 
-    function Card(props) {
-        const [open, setOpen] = React.useState(false);
+function Card(props) {
+  const [open, setOpen] = React.useState(false);
 
-        const handleClickOpen = () => {
-          setOpen(true);
-        };
-      
-        const handleClose = () => {
-          setOpen(false);
-        };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-        return  <React.Fragment>
-       <Button variant="outlined" onClick={handleClickOpen}>
-       <div className="flex justify-center items-center ">
-       <img className=" relative"  src={props.logo}/>
-      <p className='text-white text-2xl text-head2  absolute'> 
-      
-      <h2>{props.name}</h2>  {props.button}</p>
-      
-       
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+    <Button variant="outlined" onClick={handleClickOpen}>
+      <div className="flex justify-center items-center">
+        <img className="relative" src={props.logo} alt="Company Logo" />
+        <div className="text-white text-2xl text-head2 absolute">
+          <h2>{props.companyname}</h2> {props.button}
         </div>
-      </Button>
-
-
-      <Dialog
-      className="w-1/2"
-        fullScreen
-        open={open}
-        onClose={handleClose}
-        TransitionComponent={Transition}
-        PaperProps={{
-          style: {
-            width: '100%', // Half of the page width
-            marginLeft: 'auto', // Align to the right side
-          },
+      </div>
+    </Button>
+    <Dialog
+      className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3"
+      fullScreen={window.innerWidth < 640}
+      open={open}
+      onClose={handleClose}
+      TransitionComponent={Transition}
+      PaperProps={{
+        style: {
+          width: '100%',
+        },
+      }}
+    >
+      <IconButton 
+        onClick={handleClose} 
+        style={{
+          backgroundColor: "white", 
+          position: 'sticky', 
+          height: "30px",
+          width: "30px",
+          top: '10px', 
+          left: '380px',
+          color: 'black',
+          zIndex: 10,
         }}
       >
-        <AppBar sx={{ position: 'relative' }}>
-          <Toolbar>
-            <IconButton
-              edge="right"
-              color="inherit"
-              onClick={handleClose}
-              aria-label="close"
-            >
-              <CloseIcon />
-            </IconButton>
-            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-            <p>{props.center}</p>
-            </Typography>
-            
-          </Toolbar>
-        </AppBar>
-        <List>
-          <ListItemButton>
-            <ListItemText primary="" secondary="" />
-          </ListItemButton>
-          <p>{props.active}</p> 
-       
-          <Divider />
-          <ListItemButton>
-            <ListItemText
-            
-              primary=""
-              secondary=""
-            />
-             <img className="h-2xl w-3xl"  src={props.img}/>
-          </ListItemButton>
-        </List>
-      </Dialog>
-    </React.Fragment>
+        <CloseIcon />
+      </IconButton>
       
+      <div className=" -mt-8">
+        <img className="w-full" src={props.logo} alt="Company Logo" />
+      </div>
       
-        
-      }
+      <div className="p-5">
+        <h2 className="text-3xl font-bold">{props.companyname}</h2>
+        <h1 className="text-4xl font-black">{props.industryname}</h1>
+        <p className="text-2xl mt-5">{props.paragraph}</p>
+      </div>
+      <div className="p-5">
+        <h2 className="text-3xl font-bold">Service Provided</h2>
+        <div className="flex flex-col gap-5 w-full">
+          <div className="flex flex-col sm:flex-row gap-5">
+            <img className="w-full sm:w-1/2" src={props.img1} />
+            <img className="w-full sm:w-1/2" src={props.img2}  />
+          </div>
+          <div className="flex flex-col sm:flex-row gap-5">
+            <img className="w-full sm:w-1/2" src={props.img3}  />
+            <img className="w-full sm:w-1/2" src={props.img4} />
+          </div>
+        </div>
+        <div className="p-5 flex flex-col gap-5">
+          <h1 className="text-3xl font-bold">Would you like to know more about the project?</h1>
+          <p className="text-2xl mt-5">
+            To find more about our work or any of our other brands, get in touch. Mail us on <span>{props.email}</span> or call <span>{props.phone}</span>.
+          </p>
+        </div>
+      </div>
 
-     
-
+      <div className="sticky bottom-0 border-t-1 p-5 bg-white">
+        <button className="w-56 h-12 border-2">
+          Let's talk <ArrowForwardIcon />
+        </button>
+      </div>
+    </Dialog>
+  </React.Fragment>
+  );
+}
 
 export default Card;
--- src/Componont/Home/Homepage/Serve/Serve.jsx
+++ src/Componont/Home/Homepage/Serve/Serve.jsx
@@ -1,38 +1,71 @@
-import React, { useState } from "react";
-import Sideleft from "../../../../assets/sideleft.png";
-import Sideright from "../../../../assets/sideright.png";
+import React, { useState, useEffect } from "react";
+import Sideleft from "../../../../assets/Homepage/leftlogo.png";
+import Sideright from "../../../../assets/Homepage/rightlogo.png";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import Stack from "@mui/material/Stack";
 import Increaser from "./increaser";
-import "./serve.css"
+import "./serve.css";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 function Serve() {
-  
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  useEffect(() => {
+    if (!hasAnimated) {
+      setHasAnimated(true);
+    }
+  }, [hasAnimated]);
+
+  const variants = {
+    initial: {
+      y: 60,
+      opacity: 0,
+    },
+    animate: {
+      y: 1,
+      opacity: 1,
+      transition: {
+        duration: 5,
+        staggerChildren: 1,
+      },
+    },
+  };
+
   return (
     <>
-     <div className="flex flex-col items-center   mb-10">
-  <div id="servebg" className="flex flex-col md:flex-row justify-between items-center w-full mt-16 bg-Amber">
-    <div className="hidden md:block">
-      <img className="h-56" src={Sideright} alt="Side Right" />
-    </div>
-    <div className="w-full md:w-auto flex justify-center md:justify-start">
-      <Increaser />
-    </div>
-    <div className="hidden md:block">
-      <img className="h-56" src={Sideleft} alt="Side Left" />
-    </div>
-  </div>
-  <div id="Serve" className="h-24 w-10/12 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-black text-white bg-increaser flex flex-col md:flex-row items-center justify-between rounded-xl -mt-10 p-4">
-    <h1 className="mb-4 md:mb-0">Let's Get Your Project Started</h1>
-    <Link to="/Services">
-      <Stack spacing={2} direction="row">
-        <Button variant="contained">View All</Button>
-      </Stack>
-    </Link>
-  </div>
-</div>
-
+      <div className="flex flex-col items-center mb-10">
+        <div
+          id="servebg"
+          className="flex flex-col md:flex-row justify-between items-center w-full mt-16 bg-Amber"
+        >
+          <div className="hidden md:block">
+            <img className="h-64" src={Sideright} alt="Side Right" />
+          </div>
+          <div className="w-full md:w-auto flex justify-center md:justify-start">
+            <Increaser />
+          </div>
+          <div className="hidden md:block">
+            <img className="h-64" src={Sideleft} alt="Side Left" />
+          </div>
+        </div>
+        {hasAnimated && (
+          <motion.div
+            id="Serve"
+            className="h-24 w-10/12 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-black text-white bg-increaser flex flex-col md:flex-row items-center justify-between rounded-xl -mt-10 p-4"
+            variants={variants}
+            initial="initial"
+            animate="animate"
+          >
+            <h1 className="mb-4 md:mb-0">Let's Get Your Project Started</h1>
+            <Link to="/Services">
+              <Stack spacing={2} direction="row">
+                <Button variant="contained">View All</Button>
+              </Stack>
+            </Link>
+          </motion.div>
+        )}
+      </div>
     </>
   );
 }
